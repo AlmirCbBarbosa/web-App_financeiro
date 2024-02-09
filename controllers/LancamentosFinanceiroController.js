b7148300764fa7fb6f82579a18318548d6fd43e5
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2024-02-08 21:55:26 -0300

    rota get da página lacamento.ejs finalizada e intregada

--- controllers/LancamentosFinanceiroController.js
+++ controllers/LancamentosFinanceiroController.js
@@ -5,11 +5,25 @@ class LancamentoFinanceiroController{
         res.render('index', {title: 'Home Controle Financeiro'});
     };
 
-    static async lancamento(req, res){
-        const conn = await connect();
-        const [rows] = await conn.query('SELECT *FROM eventos_financeiros;');
-        console.log(rows[0].formaDePagamento);
-        res.render('lancamento', {title:'lançamento'});
+    static async lancamento(req, res){    
+        const conn = await connect();    
+        const [rows] = await conn.query('SELECT *FROM eventos_financeiros;');       
+        
+        //-----------------trabalhando os dados----------------//
+        rows.forEach(transacao =>{
+            //data no formato dd/mm/aaaa
+            let isoData = transacao.dataEvento;
+            let data = new Date(isoData);
+            data = data.toISOString();
+            data = data.split("T")[0].split("-").reverse();
+            data = data[0] + '/' + data[1] + '/' + data[2];            
+            transacao.dataEvento = data;
+            
+            //valor com duas casas decimais
+            transacao.valor = transacao.valor.toFixed(2);            
+        });
+        
+        res.render('lancamento', {rows});
     }
 }
 
